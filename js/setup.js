4ef4ce5c57885832a21b771d5083a6e3dfb21be4
flaviofigueiredo <flaviofigueiredopinto@gmail.com>
2021-07-23 20:17:25 -0300

    projetonetflix

---
+++ js/setup.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0:{
+                item:1
+            },
+            600:{
+                item:5
+            },
+            1000:{
+                item:7
+            }
+            
+            
+        }
+    });
+  
+
+
+
+
+
+});
